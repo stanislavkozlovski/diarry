9d286056823efec829a94baa4aaaf124b60d01ac
Enether <familyguyuser192@windowslive.com>
2017-04-05 12:09:06 +0300

    Changed DiaryDetails for a stateless functional component and added DiaryEntry in its place

--- react/js/ClientApp.js
+++ react/js/ClientApp.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { HashRouter, Match } from 'react-router'
 
-import DiaryEntry from './DiaryEntry.js'
+import DiaryDetails from './DiaryDetails.js'
 
 import '../public/css/5grid/core-desktop.css'
 import '../public/css/5grid/core-1200px.css'
@@ -18,7 +18,7 @@ const App = React.createClass({
         <div className='app'>
           <div id='content' className='mobileUI-main-content'>
             <div id='content-inner'>
-              <Match exactly pattern='/entry/:id' component={DiaryEntry} />
+              <Match exactly pattern='/entry/:id' component={DiaryDetails} />
             </div>
           </div>
         </div>
---
+++ react/js/DiaryDetails.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import axios from 'axios'
+import DiaryEntry from './DiaryEntry.js'
+const { shape, string } = React.PropTypes
+// maybe turn into a stateless component and have a higher comp do AJAX :)
+const DiaryDetails = React.createClass({
+  propTypes: {
+    params: shape({
+      id: string
+    })
+  },
+  componentDidMount () {
+    /* query the backend for details for a specific DiaryEntry and show it to the user */
+    axios.get(`http://localhost:8000/api/entries/${this.props.params.id}`)
+    .then(resp => {
+      this.setState(resp.data)
+    }).catch(err => {
+      if (err.response) {
+        if (err.response.status === 404) {
+          this.setState({error: `No entry with ID ${this.props.params.id} exists!`})
+        } else {
+          console.log(`Unexpected error: ${err}`)
+        }
+      } else {
+        console.log(`Unexpected error: ${err}`)
+      }
+    })
+  },
+
+  render () {
+    if (this.state && this.state.body) {
+      return (
+        <DiaryEntry {...this.state} />
+      )
+    } else if (this.state && this.state.error) {
+      return <h1 className='alert alert-danger'> {this.state.error} </h1>
+    } else {
+      return <h1> Loading </h1>
+    }
+  }
+})
+
+export default DiaryDetails
