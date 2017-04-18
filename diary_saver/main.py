7113743d0a26a03c1d385ce879c78237ec417a8f
Enether <familyguyuser192@windowslive.com>
2017-04-18 17:16:22 +0300

    Added Diary Entry formatting

--- diary_saver/main.py
+++ diary_saver/main.py
@@ -1,5 +1,6 @@
 import settings
 import models
+import textwrap
 
 
 def fetch_all_entries_sorted_by_date():
@@ -10,4 +11,21 @@ def fetch_all_entries_sorted_by_date():
         .order_by(models.DiaryEntry.creation_date.asc(), models.DiaryEntry.creation_time.asc())\
         .all()
 
-print(fetch_all_entries_sorted_by_date())
+
+def format_diary_entry(entry: models.DiaryEntry) -> str:
+    """
+    Formats a single Diary Entry to the wanted format to be outputted in the .txt file
+    """
+    entry_id = entry.id
+    title = entry.title
+    body = entry.body
+    date = model.creation_date.strftime("%d %B %Y")
+    time = model.creation_time.strftime("%H:%M")
+
+    formatted_entry = f"""\t\t\t\t\t\t\t{title}
+#{entry_id} - {date} {time}
+
+{body}
+    """
+    return formatted_entry
+
--- diary_saver/tests.py
+++ diary_saver/tests.py
@@ -1,6 +1,10 @@
 import unittest
-from main import fetch_all_entries_sorted_by_date
+from unittest.mock import MagicMock
+from datetime import date, time
+
+from main import fetch_all_entries_sorted_by_date, format_diary_entry
 from models import DiaryEntry
+
 
 class Tests(unittest.TestCase):
     def test_fetch_all_entries_sorted_by_date(self):
@@ -14,6 +18,16 @@ class Tests(unittest.TestCase):
             else:
                 self.assertTrue(prev_entry.creation_date < curr_entry.creation_time)
 
+    def test_format_diary_entry_formats_correctly(self):
+        diary_entry = MagicMock(title="Train", body="Training Insane", id=1,
+                                creation_date=date(month=4, day=4, year=2017),
+                                creation_time=time(hour=14, minute=50))
+        expected_output = """							Train
+#1 - 04 April 2017 14:50
+
+Training Insane"""
+
+        self.assertIn(expected_output, format_diary_entry(diary_entry))
 
 if __name__ == '__main__':
     unittest.main()
